e5b34db7c835059d1a37faca72a6012644a8bff1
renoanascimento <renoaalmeida@gmail.com>
2021-03-01 18:57:57 -0300

    Criação do capítulo - Interfaces

---
+++ 13) Interfaces/Aulas/Aula 195 - Sem Interface/RentalCars/Entities/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿namespace RentalCars.Entities
+{
+    class Vehicle
+    {
+        public string Model { get; set; }
+
+        public Vehicle(string model)
+        {
+            Model = model;
+        }
+    }
+}
---
+++ 13) Interfaces/Aulas/Aula 195 - Sem Interface/RentalCars/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using RentalCars.Entities;
+using RentalCars.Services;
+
+namespace RentalCars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+            Console.Write("Pickup (dd/MM/yy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return (dd/MM/yy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Enter price per hour: ");
+            double pricePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter price per day: ");
+            double pricePerDay = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            RentalService rentalService = new RentalService(pricePerHour, pricePerDay);
+
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("INVOICE: ");
+            Console.WriteLine(carRental.Invoice);
+        }
+    }
+}
