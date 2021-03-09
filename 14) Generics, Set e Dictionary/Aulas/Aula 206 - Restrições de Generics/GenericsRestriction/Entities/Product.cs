b4de91cb55b9d6c039f4cf8e4996285148ca49c3
renoanascimento <renoaalmeida@gmail.com>
2021-03-08 21:55:15 -0300

    Upload do capítulo Generics, Set e Dictionary

---
+++ 14) Generics, Set e Dictionary/Aulas/Aula 206 - Restrições de Generics/GenericsRestriction/Entities/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace GenericsRestriction.Entities
+{
+    class Product : IComparable
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ", "
+                + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Product))
+            {
+                throw new ArgumentException("Compare error! Argument is not a product!");
+            }
+
+            Product other = obj as Product;
+
+            return Price.CompareTo(other.Price);
+        }
+    }
+}
---
+++ 14) Generics, Set e Dictionary/Aulas/Aula 207 - GetHashCode e Equals/HashCodeEquals/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using HashCodeEquals.Entities;
+
+namespace HashCodeEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com" };
+            Client b = new Client { Name = "Alex", Email = "maria@gmail.com" };
+
+            Console.WriteLine(a.Equals(b));
+            Console.WriteLine(a == b);
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
