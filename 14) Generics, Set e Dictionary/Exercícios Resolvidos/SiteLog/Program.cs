08f946797056957a2de344bb629092093290f4d4
renoanascimento <renoaalmeida@gmail.com>
2021-03-10 19:07:10 -0300

    Upload de exercício resolvido sobre conjuntos

---
+++ 14) Generics, Set e Dictionary/Exercícios Resolvidos/SiteLog/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using SiteLog.Entities;
+
+namespace SiteLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            HashSet<LogRecord> logs = new HashSet<LogRecord>();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] vectLine = sr.ReadLine().Split(' ');
+                        string userName = vectLine[0];
+                        DateTime instant = DateTime.Parse(vectLine[1]);
+                        LogRecord log = new LogRecord(userName, instant);
+                        logs.Add(new LogRecord(userName, instant));
+                    }
+                }
+
+                Console.WriteLine("Total users: " + logs.Count);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
