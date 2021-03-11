9ad47b11d40c490d5270d0b4557066c6908419ea
renoanascimento <renoaalmeida@gmail.com>
2021-03-11 18:33:20 -0300

    Upload do capítulo Tópicos Especiais em C# - Parte 02

---
+++ 15) Tópicos Especiais em C# - Parte 02/Aulas/Aula 217 - Extension Methods/ExtensionDate/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExtensionDate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2021, 03, 11, 08, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+
+            string s1 = "Good morning dear students!";
+
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
