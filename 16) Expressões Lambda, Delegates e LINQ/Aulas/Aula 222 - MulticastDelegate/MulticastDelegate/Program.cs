63413e2f4e2583491bc2cd4cb032f618aa65f0ca
renoanascimento <renoaalmeida@gmail.com>
2021-05-19 19:09:01 -0300

    Upload das Aulas de Tipos de Delegates

---
+++ 16) Expressões Lambda, Delegates e LINQ/Aulas/Aula 222 - MulticastDelegate/MulticastDelegate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using IntroDelegates.Services;
+
+namespace IntroDelegates
+{
+    class Program
+    {
+        delegate void BinaryNumericOperation(double n1, double n2);
+
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+            //BinaryNumericOperation op = new BinaryNumericOperation(CalculationService.Sum);
+
+            op.Invoke(a, b);
+            //op(a, b);
+        }
+    }
+}
---
+++ 16) Expressões Lambda, Delegates e LINQ/Aulas/Aula 223 - Predicates/Predicates/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Predicates.Entities;
+
+namespace Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.RemoveAll(ProductTest);
+            //list.RemoveAll(p => p.Price >= 100.0);
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        public static bool ProductTest(Product p)
+        {
+            return p.Price >= 100.0;
+        }
+    }
+}
