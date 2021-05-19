ff74e6de20008a3e200e8f90d7649beb10a3b8a2
renoanascimento <renoaalmeida@gmail.com>
2021-05-19 18:01:38 -0300

    Upload das Aulas Iniciais do Capítulo

---
+++ 16) Expressões Lambda, Delegates e LINQ/Aulas/Aula 221 - IntroDelegates/IntroDelegates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using IntroDelegates.Services;
+
+namespace IntroDelegates
+{
+    class Program
+    {
+
+        delegate double BinaryNumericOperation(double n1, double n2);
+
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+            //BinaryNumericOperation op = new BinaryNumericOperation(CalculationService.Sum);
+
+
+            double result = op.Invoke(a, b);
+
+            Console.WriteLine(result);
+        }
+    }
+}
